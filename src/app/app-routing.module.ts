2161cdfd79527bf71e8e3b613cd88fa5c395dc13
Aline <aline.isilvadamiao@gmail.com>
2022-04-10 19:13:13 -0300

    criando módlo router

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+import { NovaTransferenciaComponent } from './nova-transferencia/nova-transferencia.component';
+import { ExtratoComponent } from './extrato/extrato.component';
+import { NgModule } from "@angular/core";
+import { RouterModule } from "@angular/router";
+
+import { Routes } from '@angular/router';
+
+//objeto de rotas e array
+export const routes: Routes = [
+  {path: '', redirectTo: 'extrato', pathMatch:'full'},
+  {path: 'extrato', component:ExtratoComponent},
+  {path: 'nova-transferencia', component:NovaTransferenciaComponent},
+]
+
+//importação de módulo sando o método estático forRoot e em seguida importação
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports:[RouterModule]
+})
+
+export class AppRotingModule{
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { ExtratoComponent } from './extrato/extrato.component';
 import { registerLocaleData } from '@angular/common';
 import localePt from '@angular/common/locales/pt';
 import { HttpClientModule } from '@angular/common/http';
+import { AppRotingModule } from './app-routing.module';
 
 registerLocaleData(localePt, 'pt');
 
@@ -22,6 +23,7 @@ registerLocaleData(localePt, 'pt');
     BrowserModule,
     FormsModule,
     HttpClientModule,
+    AppRotingModule
   ],
   providers: [
     [{provide: LOCALE_ID, useValue: 'pt' }],
